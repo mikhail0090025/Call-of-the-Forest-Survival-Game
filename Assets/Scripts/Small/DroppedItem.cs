e3cd3f6f9953517b97a4b15bd2ffab52a7afbf4c
Mikhail Ivashchenko <52649381+mikhail0090025@users.noreply.github.com>
2024-05-09 12:24:27 +0200

    Furnace added

---
+++ Assets/Scripts/Small/DroppedItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedItem : MonoBehaviour
+{
+    [SerializeField] TextOnWatching TextOnWatching;
+    [SerializeField] CollectableObject CollectableObjectScript;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
